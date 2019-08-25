d44c2764067257182359ce548f2998ab7318cfd6
Marlon Lopes <marlon.lopes@hp.com>
2019-08-25 16:33:52 -0300

    docker version 10

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World  2 muito legal!');
+  response.end('Hello World  2 version 10!');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
